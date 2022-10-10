a18e9085562a1d84582b4ba354d1af435674976e
Jan Karcz <85148746+Jank201@users.noreply.github.com>
2022-10-10 12:51:30 -0400

    Update MathSolver.py

--- Battleship/MainFiles/MathSolver.py
+++ Battleship/MainFiles/MathSolver.py
@@ -2,9 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from MainGameMath import BattleshipAI, SHIPLENGTH, BOARDSQUARES
 from Graph import plot
-from numba import jit, cuda
-
-
 
 
 BOARDSIZE = BOARDSQUARES
